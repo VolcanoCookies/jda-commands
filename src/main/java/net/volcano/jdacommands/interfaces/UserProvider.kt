31e3dac5a66296996b345d36f7c073731744ba84
VolcanoCookies <franek.kg@gmail.com>
2021-06-04 16:39:52 +0200

    Added getUserEntry function to enable extension of command event by the user to provide their own userentry object

--- src/main/java/net/volcano/jdacommands/interfaces/UserProvider.kt
+++ src/main/java/net/volcano/jdacommands/interfaces/UserProvider.kt
@@ -1,52 +1,66 @@
-package net.volcano.jdacommands.interfaces;
+package net.volcano.jdacommands.interfaces
 
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.Guild
+import net.dv8tion.jda.api.entities.User
 
-public interface UserProvider {
-	
+interface UserProvider {
+
 	/**
 	 * Check if a user is banned from running commands
 	 *
 	 * @param userId the user to check
-	 * @return {@code true} if the user is banned from running ANY commands
+	 * @return `true` if the user is banned from running ANY commands
 	 */
-	boolean isCommandBanned(String userId);
-	
+	fun isCommandBanned(userId: String?): Boolean
+
 	/**
 	 * Check if a user is banned from running commands
 	 *
 	 * @param user the user to check
-	 * @return {@code true} if the user is banned from running ANY commands
+	 * @return `true` if the user is banned from running ANY commands
 	 */
-	default boolean isCommandBanned(User user) {
-		return isCommandBanned(user.getId());
+	fun isCommandBanned(user: User): Boolean {
+		return isCommandBanned(user.id)
 	}
-	
+
 	/**
 	 * Check if a user is banned from running commands in a specific guild
 	 *
 	 * @param userId  the user to check
 	 * @param guildId the guild to check in
-	 * @return {@code true} if the user is banned from running ANY commands in the guild
+	 * @return `true` if the user is banned from running ANY commands in the guild
 	 */
-	boolean isCommandBanned(String userId, String guildId);
-	
+	fun isCommandBanned(userId: String?, guildId: String?): Boolean
+
 	/**
 	 * Check if a user is banned from running commands in a specific guild
 	 *
 	 * @param user  the user to check
 	 * @param guild the guild to check in
-	 * @return {@code true} if the user is banned from running ANY commands in the guild
+	 * @return `true` if the user is banned from running ANY commands in the guild
 	 */
-	default boolean isCommandBanned(User user, Guild guild) {
-		if (guild != null) {
-			return isCommandBanned(user.getId(), guild.getId());
+	fun isCommandBanned(user: User, guild: Guild?): Boolean {
+		return if (guild != null) {
+			isCommandBanned(user.id, guild.id)
 		} else {
-			return isCommandBanned(user.getId());
+			isCommandBanned(user.id)
 		}
 	}
-	
+
+	/**
+	 * Get a user entry
+	 *
+	 * Receiver has to ensure type
+	 */
+	fun getUserEntry(user: User): Any {
+		return getUserEntry(user.id)
+	}
+
+	/**
+	 * Get a user entry
+	 *
+	 * Receiver has to ensure type
+	 */
+	fun getUserEntry(userId: String): Any
+
 }
-
-
