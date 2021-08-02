4676d83523f875290e0e9b92f15c59d5e7767a4d
VolcanoCookies <franek.kg@gmail.com>
2021-08-02 12:00:34 +0200

    Added component annotation cause forgot

--- src/main/java/net/volcano/jdacommands/model/command/arguments/implementation/codec/OffsetDateTimeCodec.java
+++ src/main/java/net/volcano/jdacommands/model/command/arguments/implementation/codec/OffsetDateTimeCodec.java
@@ -4,9 +4,11 @@ import net.volcano.jdacommands.model.ParameterData;
 import net.volcano.jdacommands.model.command.arguments.CommandArgument;
 import net.volcano.jdacommands.model.command.arguments.implementation.argument.OffsetDateTimeArgument;
 import net.volcano.jdacommands.model.command.arguments.interfaces.Codec;
+import org.springframework.stereotype.Component;
 
 import java.time.OffsetDateTime;
 
+@Component
 public class OffsetDateTimeCodec extends Codec<OffsetDateTime> {
 	
 	@Override
