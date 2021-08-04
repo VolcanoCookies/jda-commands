02d4b384caebcbd43ed2f67dd9068daab053d88c
VolcanoCookies <franek.kg@gmail.com>
2021-08-04 15:22:25 +0200

    Added min/max support to string

--- src/main/java/net/volcano/jdacommands/model/command/arguments/implementation/argument/StringArgument.java
+++ src/main/java/net/volcano/jdacommands/model/command/arguments/implementation/argument/StringArgument.java
@@ -8,6 +8,9 @@ import net.volcano.jdacommands.model.command.arguments.implementation.ArgumentPa
 @SuperBuilder
 public class StringArgument extends CommandArgument<String> {
 	
+	protected final Integer min;
+	protected final Integer max;
+	
 	/**
 	 * If the last argument, take the rest of the input
 	 */
@@ -15,6 +18,13 @@ public class StringArgument extends CommandArgument<String> {
 	
 	@Override
 	public String parseValue(ArgumentParsingData data) throws InvalidArgumentsException {
+		
+		if (min != null && data.getArg().length() < min) {
+			throw new InvalidArgumentsException(data, "String needs to be at least " + min + " characters long.");
+		} else if (max != null && data.getArg().length() > max) {
+			throw new InvalidArgumentsException(data, "String can be no more than " + max + " characters long.");
+		}
+		
 		return data.getArg();
 	}
 	
--- src/main/java/net/volcano/jdacommands/model/command/arguments/implementation/codec/StringCodec.java
+++ src/main/java/net/volcano/jdacommands/model/command/arguments/implementation/codec/StringCodec.java
@@ -1,6 +1,7 @@
 package net.volcano.jdacommands.model.command.arguments.implementation.codec;
 
 import net.volcano.jdacommands.model.ParameterData;
+import net.volcano.jdacommands.model.command.annotations.argument.Arg;
 import net.volcano.jdacommands.model.command.arguments.CommandArgument;
 import net.volcano.jdacommands.model.command.arguments.implementation.argument.StringArgument;
 import net.volcano.jdacommands.model.command.arguments.interfaces.Codec;
@@ -12,6 +13,13 @@ public class StringCodec extends Codec<String> {
 	@Override
 	protected CommandArgument<String> buildArgument(ParameterData data) {
 		var builder = StringArgument.builder();
+		
+		var arg = data.parameter.getAnnotation(Arg.class);
+		
+		if (arg != null) {
+			builder.min(arg.min() != Double.MIN_VALUE ? (int) arg.min() : null);
+			builder.max(arg.max() != Double.MAX_VALUE ? (int) arg.max() : null);
+		}
 		
 		return builder.build();
 	}
